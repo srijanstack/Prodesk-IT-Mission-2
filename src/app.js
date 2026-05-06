f76d80ea8d433b310b1a686425f0e2744a769af3
Srijan Rawat <srijanrawat3@gmail.com>
2026-05-06 17:28:59 +0530

    feat: expense list and calculations

--- src/app.js
+++ src/app.js
@@ -5,25 +5,100 @@ const expenseButton = document.getElementById("expBtn");
 const expenseNameInput = document.getElementById("expenseName");
 const expenseValueInput = document.getElementById("expenseValue");
 
-const savedSalary = localStorage.getItem("salary");
-salaryInput.value = JSON.parse(savedSalary) || null;
+const expList = document.getElementById("expList");
+const bar = document.getElementById("topExpBar")
 
-const savedExpenses = localStorage.getItem("expenses");
-let expenses = JSON.parse(savedExpenses) || [];
+const bal = document.getElementById("balance");
+const totExp = document.getElementById("totExp");
+const sal = document.getElementById("sal");
+
+let salary = JSON.parse(localStorage.getItem("salary")) || null;
+salaryInput.value = Number(salary);
+let expenses = JSON.parse(localStorage.getItem("expenses")) || [];
 
 
 salaryBtn.addEventListener('click', () => {
-    localStorage.setItem("salary", JSON.stringify(salaryInput.value));
-})
+    updateSalary();
+})    
+
+
+function updateSalary() {
+    salary = Number(salaryInput.value); 
+    localStorage.setItem("salary", JSON.stringify(salary));
+    sal.innerText = salary.toLocaleString('en-IN');
+
+    updateExpenseList(); 
+}
+
+function balance() {
+    const balance = salary - totalExpense();
+    bal.innerText = balance.toLocaleString('en-in');
+}
 
 
 expenseButton.addEventListener('click', () => {
     const expName = expenseNameInput.value;
     const expValue = expenseValueInput.value;
 
-    const totalExpenses = [...expenses, { name: expName, value: expValue  }]
-    localStorage.setItem("expenses", JSON.stringify(totalExpenses))
+    if (!expValue || !expName) {
+        return alert("Enter Expense")
+    }
+
+    expenses = [...expenses, { name: expName, value: Number(expValue) }]
+    localStorage.setItem("expenses", JSON.stringify(expenses));
+
+    updateExpenseList();
 })
+
+function updateExpenseList() {
+
+    if (expenses.length === 0) {
+        expList.innerHTML = `  <h1 class="font-semibold text-2xl text-yellow-500">No expenses. You are doing good.</h1>`
+        bar.style.display = `none`
+        totalExpense();
+        balance();
+        return
+    }
+
+    bar.style.display = `flex`
+
+    expList.innerHTML = expenses
+        .map((exp, i) => ` <li class="w-full flex ">
+                        <div class="border-b border-r border-l w-1/4 h-7 text-center">
+                            ${i + 1}.
+                        </div>
+                        <div class="border-b border-r w-1/3 h-7 text-center">
+                            ${exp.name}
+                        </div>
+                        <div class="border-b border-r  w-1/3 h-7 text-center">
+                            ${exp.value}
+                        </div>
+                        <button class="border-b  border-r w-1/10 h-7 text-center text-red-500 font-semibold border-black cursor-pointer hover:scale-105 transition duration-100" onClick="removeExpense(${i})">
+                            <i class="fa-solid fa-trash"></i>
+                        </button>
+                    </li>`)
+        .join("");
+
+    totalExpense();
+    balance();
+}
+
+function removeExpense(i) {
+    expenses.splice(i, 1);
+    localStorage.setItem("expenses", JSON.stringify(expenses));
+    updateExpenseList();
+}
+
+function totalExpense() {
+    const totalExpenses = expenses.reduce((acc, curr) => {
+        return acc + curr.value;
+    }, 0);
+    totExp.innerText = totalExpenses.toLocaleString('en-In');
+    return totalExpenses;
+}
+
+updateSalary();
+updateExpenseList();
 
 
 
